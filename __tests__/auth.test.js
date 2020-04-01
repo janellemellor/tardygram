412696f76c31b395f017e1b60e8f971c3b560318
janelle mellor <janellemellor@gmail.com>
2020-03-31 17:25:58 -0700

    user signup route now passing

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -4,23 +4,21 @@ require('../db/data-helpers');
 
 describe('auth routes', () => {
   it('signs up a user', () => {
-    expect(true).toEqual(true);
-    // return request(app)
-    //   .post('/api/v1/auth/signup')
-    //   .send({
-    //     username: 'fox',
-    //     password: 'hoorayitsmyadoptionday',
-    //     profilePhotoUrl: 'https://placedog.net/500'
-    //   })
-    //   .then(res => {
-    //       console.log(res.body);
-    //     expect(res.body).toEqual({
-    //       _id: expect.any(String),
-    //       username: 'fox',
-    //       profilePhotoUrl: 'https://placedog.net/500',
-    //       __v: 0
-    //     });
-    //   });
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'fox',
+        password: 'hoorayitsmyadoptionday',
+        profilePhotoUrl: 'https://placedog.net/500'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'fox',
+          profilePhotoUrl: 'https://placedog.net/500',
+          __v: 0
+        });
+      });
   });    
 });
 
