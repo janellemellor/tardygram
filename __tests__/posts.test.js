8310a32c50a1633d9ef1f849da94ad9469fb176f
janelle mellor <janellemellor@gmail.com>
2020-04-01 16:14:20 -0700

    add test for find post by id

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -26,27 +26,42 @@ describe('post routes', () => {
         });
       });
   });
+
+  it('gets all posts', async() => {
+    const posts = await getPosts();
+
+    return request(app)
+      .get('/api/v1/posts')
+      .then(res => {
+        expect(res.body).toEqual(posts);
+      });
+  });
+
+  it('finds a post by id', async() => {
+    const user = await getUser({ username: 'fakeUser' });
+    const post = await getPost({ user: user._id });
+
+    return getAgent()
+      .get(`/api/v1/posts/${post._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...post,
+          user
+        });
+      });
+  });
+
 });
 
-it('gets all posts', async() => {
-  const posts = await getPosts();
 
-  return request(app)
-    .get('/api/v1/posts')
-    .then(res => {
-      expect(res.body).toEqual(posts);
-    });
-});
-
-
-
-// * `GET /posts`
-//   * responds with a list of posts
 // * `GET /posts/:id`
 //   * responds with a post by id
 //   * should include the populated user
 //   * should include all comments associated with the post (populated with commenter)
 //     * HINT: You'll need to make two separate queries and a `Promise.all`
+
+
+
 // * `PATCH /posts/:id`
 //   * requires authentication
 //   * only can update the post caption
