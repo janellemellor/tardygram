78c65b3ed655c6a8eb309669a887d03010c9c011
janelle mellor <janellemellor@gmail.com>
2020-04-01 13:45:45 -0700

    add posts to seed

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,8 @@
 const chance = require('chance').Chance();
 const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
-module.exports = async({ usersToCreate = 5 } = {}) => {
+module.exports = async({ usersToCreate = 5, postsToCreate = 10 } = {}) => {
   const loggedInUser = await User.create({
     username: 'fakeUser',
     password: 'iliketoeatapplesandbananas',
@@ -12,5 +13,12 @@ module.exports = async({ usersToCreate = 5 } = {}) => {
     username: chance.email(),
     password: chance.string(10),
     profilePhotoUrl: chance.url({ path: 'images' }) 
+  })));
+
+  const posts = await Post.create([...Array(postsToCreate)].map(() => ({
+    user: chance.pickone(users)._id,
+    photoUrl: chance.url({ path: 'images' }),
+    caption: chance.sentence(),
+    tags:[chance.hashtag()]
   })));
 };
